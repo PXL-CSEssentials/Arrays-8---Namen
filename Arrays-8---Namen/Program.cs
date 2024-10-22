26afc1d0ec3e7821072492ee5f8e3f365d4196f3
Koen Bloemen <koen.bloemen@pxl.be>
2024-10-22 17:37:01 +0200

    final

---
+++ Arrays-8---Namen/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Hoeveel namen wens je te beheren? ");
+int numberOfNames = int.Parse(Console.ReadLine());
+string[] names = new string[numberOfNames];
+
+Console.WriteLine("==== Namen in array stockeren en afdrukken. ====\r\n");
+for (int i = 0; i < names.Length; i++)
+{
+    Console.Write($"Geef naam {i + 1}/{names.Length} : ");
+    names[i] = Console.ReadLine();
+}
+
+Console.WriteLine("\r\n\r\n==== Namen afdrukken. ====\r\n");
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine($"Naam {i + 1} is  {names[i].ToUpper()}");
+}
+
+// Of via foreach
+/*
+int index = 1; 
+foreach (string name in names) 
+{ 
+    Console.WriteLine($"Naam {index} is  {name.ToUpper()}"); 
+    index++; 
+}
+*/
+Console.ReadLine();
